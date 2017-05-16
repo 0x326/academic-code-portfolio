80052150afe94557353ad57ceaf91f6d261dda53
John Meyer <0x326@users.noreply.github.com>
2017-05-16 15:35:09 -0400

    Add CSE 102 code

---
+++ CSE 102/Palindrome Checker Program.s
@@ -0,0 +1,86 @@
+; Written for https://schweigi.github.io/assembler-simulator/
+; Prints 'T' if the given variable is a palindrome, 'F' otherwise
+
+  JMP start
+var:
+  DB "able was I ere I saw elba"
+  DB 0
+
+start:
+  MOV A, var
+  CALL findEnd ; Find end of string
+  MOV B, A
+  MOV A, var
+  CALL computePalindrome
+  MOV A, C
+  CALL printSuccessValue
+  HLT
+
+findEnd:
+  ; param: A: start of variable
+  ; return: A: end of variable
+
+  PUSH B
+  PUSH C
+  MOV B, 0
+.loop:
+  MOV C, [A]
+  INC A
+
+  CMP C, B
+  JNZ .loop
+
+  DEC A
+  POP C
+  POP B
+  RET
+
+computePalindrome:
+  ; param A: start of variable
+  ; param B: end of variable
+  ; return C: zero if false
+
+  PUSH A
+  PUSH B
+  DEC B
+.loop2:
+  CMP A, B ; Check to see whether we have breached the midpoint
+  JAE .computePalindromeYes
+
+  MOV C, [A] ; Copy values
+  MOV D, [B]
+  INC A
+  DEC B
+
+  CMP C, D ; Compare values
+  JZ .loop2
+
+  MOV C, 0
+  JMP .computePalindromeEnd
+.computePalindromeYes:
+  MOV C, 1
+  ; fall through
+
+.computePalindromeEnd:
+  POP B
+  POP A
+  RET
+
+printSuccessValue:
+  ; param A: success value (0 if false)
+
+  PUSH A
+
+  CMP A, 0 ; Determine whether A is zero
+  JZ .printSuccessValueFailure
+
+  MOV [0xE8], 'T' ; If not zero, print 'T'
+  JMP .printSuccessValueEnd
+
+.printSuccessValueFailure:
+  MOV [0xE8], 'F' ; Otherwise, print 'F'
+  ; Fall through
+
+.printSuccessValueEnd:
+  POP A
+  RET
