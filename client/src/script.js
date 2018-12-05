1fdbb3b54209ede9fb0ce19dfd1caa7fe3e4c28d
John Meyer <0x326@users.noreply.github.com>
2018-12-05 09:06:45 -0500

    Move HTML back to .html

--- client/src/script.js
+++ client/src/script.js
@@ -3,14 +3,6 @@
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css'
 require('bootstrap')
-<form id = "userForm" action = "http://ceclnx01.cec.miamioh.edu/~kauchaaj/cse383/cse-383-final-group-project/server/src/index.php">
-Enter username:
-	<input form = "text">
- 
-  Enter password:
-  <input form = "text">
-  <input type="submit">
-</form>
 
 xmlhttp.open("POST", "http://ceclnx01.cec.miamioh.edu/~kauchaaj/cse383/cse-383-final-group-project/server/src/index.php", true);
 
