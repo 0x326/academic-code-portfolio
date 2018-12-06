08d600dfa3b2cbe6dcbfd54a1c6c30c23ecb63d3
Alexander Kauchak <kauchaaj@miamioh.edu>
2018-12-06 15:34:55 -0500

    Adding skeleton to REST API ajax methods

--- client/src/script.js
+++ client/src/script.js
@@ -5,7 +5,9 @@ import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
-async function sendAjaxRequest (ajaxOptions) {
+const apiBaseUri = 'http://localhost:8080/rest.php'
+
+async function sendAjaxRequest(ajaxOptions) {
   // Convert jQuery thenables into native Promises
   const data = await Promise.resolve($.ajax(ajaxOptions))
   // if (data.status !== 'OK') {
@@ -14,39 +16,117 @@ async function sendAjaxRequest (ajaxOptions) {
   return data
 }
 
-async function submitCredentials (username, password, retryLimit = 3) {
-  if (retryLimit < 0) {
-    throw new Error('retryLimit must be non-negative')
-  }
-
+async function submitCredentials(username, password) {
   const ajaxOptions = {
     method: 'POST',
-    url: '/home/kauchaaj/public_html/cse383/cse-383-final-group-project/server/src/index.php',
+    url: `${apiBaseUri}/v1/user`,
     data: JSON.stringify({
-      username,
-      password
+      user: username,
+      password,
     }),
     contentType: 'application/json',
     dataType: 'json'
   }
 
-  let ajaxSubmission = sendAjaxRequest(ajaxOptions)
 
-  for (let i = 0; i < retryLimit; i += 1) {
-    ajaxSubmission = ajaxSubmission.catch(() => sendAjaxRequest(ajaxOptions))
+
+  const userToken = await sendAjaxRequest(ajaxOptions)
+
+  const {
+    status,
+    msg,
+    token,
+  } = userToken
+
+  if (status !== 'OK') {
+    throw new Error(msg);
   }
-  return ajaxSubmission
-  // .catch(() => $('#error').text('Form cannot be submitted since server is unavailable'))
-  // .then(({ data }) => {
-  //   $('#formDiv').hide()
-  //   $('#submissions-table-div').show()
-  //   populateTableData('#submissions-table', data)
-  //   $('#error').text('')
-  // })
+
+  return token
 }
 
-async function requestData () {
-  return sendAjaxRequest()
+
+
+async function getItems() {
+  const itemDemand = {
+    method: 'GET',
+    url: `${apiBaseUri}/v1/items`,
+    contentType: 'application/json',
+    dataType: 'json'
+  }
+  const holder = sendAjaxRequest(itemDemand)
+
+  const {
+    status,
+    msg,
+    items,
+  } = objectHold
+
+  return items
+}
+
+async function getConsumedItems() {
+  const itemsConsumed = {
+    method: 'GET',
+    url: `${apiBaseUri}/items/token`,
+    contentType: 'application/json',
+    dataType: 'json'
+  }
+  const consumed = sendAjaxRequest(itemsConsumed)
+
+  const {
+    status,
+    pk,
+    msg: text,
+    item,
+    timestamp,
+  } = consumedCollection
+
+  if(status !=='OK'){
+    throw new Error(msg)
+  }
+  return itemCollection
+}
+
+async function getItemSummary() {
+  const itemsSummarized = {
+    method: 'GET',
+    url: `${apiBaseUri}/v1/itemsSummary/token`,
+    contentType: 'application/json',
+    dataType: 'json'
+  }
+
+  const summarized = sendAjaxRequest(itemsSummarized);
+
+  const {
+status,
+    msg,
+    items[item, count =0]
+
+  } = summarizedCollection
+  return summarizedCollection
+}
+
+async function updateItem() {
+  const itemsUpdated= {
+    method: 'POST',
+    url: `${apiBaseUri}/v1/items`,
+    data: JSON.stringify({
+      token,
+      itemFK
+    }),
+    contentType: 'application/json',
+    dataType: 'json'
+  }
+
+  const updated = sendAjaxRequest(itemsUpdated);
+  const {
+    status,
+    msg
+  } = updateCollection
+  if(status !== 'OK'){
+    throw new Error(msg)
+  }
 }
 
 $(document).ready(() => {
@@ -54,19 +134,28 @@ $(document).ready(() => {
   $('#login-form').submit((evt) => {
     evt.preventDefault()
     const formData = {}
-    for (const { name, value } of $('#login-form').serializeArray()) {
+    for (const {name, value} of $('#login-form').serializeArray()) {
       formData[name] = value
     }
     const {
       username,
       password
     } = formData
-    submitCredentials(username, password)
-      .catch(() => console.log('TODO: Tell the user his password is wrong'))
-      .then(() => requestData())
-      .then((/* data */) => {
-        $('#content')
-        // TODO: Do something
-      })
+
+    const errorMessageElem = $('#error-message')
+
+    const login = submitCredentials(username, password)
+      .catch(() => errorMessageElem.text('Invalid login').show())
+      .then(() => errorMessageElem.hide())
+    Promise.all([
+      login.then(() => getItems())
+      //append to the following line.
+        .then(items => items.forEach(({ pk, item }) => $('<button></button>').text(item).click(() => updateItem(pk)))),
+      login.then(() => getConsumedItems())
+        .then(() => $()),
+      login.then(() => getItemSummary())
+        .then(() => $()),
+    ])
+      .catch(() => errorMessageElem.text('Error getting data').show())
   })
 })
